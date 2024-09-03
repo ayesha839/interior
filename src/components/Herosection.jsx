2997f7a2f52159affc3da91fa0049f986d9b9c45
ayesha839 <ayesha83984@gmail.com>
2024-09-04 00:34:01 +0500

    first commit

---
+++ src/components/Herosection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Carousel from "./Carousel";
+import Link from "next/link";
+
+const Herosection = () => {
+  return (
+    <div className="">
+      <div>
+        <Carousel />
+      </div>
+    </div>
+  );
+};
+
+export default Herosection;
